e4e175b4cd29f942169ca794494917da8690d564
Sanikant Kushwaha <141299594+sanikant20@users.noreply.github.com>
2023-09-01 14:49:18 +0545

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -26,50 +26,6 @@ db.once('open', async () => {
     });
 
 
-// const router = require("./Routes/DisplayData");
-
-// const mongooseURI = 'mongodb+srv://kushsani508:gofood508@cluster0.ic9brxy.mongodb.net/gofoodmern?retryWrites=true&w=majority';
-// const mongooseURI = 'mongodb://kushsani508:gofood508@ac-ger2m7f-shard-00-00.ic9brxy.mongodb.net:27017,ac-ger2m7f-shard-00-01.ic9brxy.mongodb.net:27017,ac-ger2m7f-shard-00-02.ic9brxy.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-b25mtj-shard-0&authSource=admin&retryWrites=true&w=majority'
-// const SALT = 10;
-
-// const ConnectDB = async () => {
-//     try {
-//         await mongoose.connect(mongooseURI, { useNewUrlParser: true });
-//         console.log("MongoDB Connected");
-//         const fetch_data = await mongoose.connection.db.collection("food_items");
-//         const data = await fetch_data.find({}).toArray()
-//         console.log(data)
-//     } catch (error) {
-//         console.error("Something went wrong:", error);
-//     }
-// }
-// module.exports = ConnectDB;
-
-
-// //Code to connect with mongodb compass
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to MongoDB');
-//         const fetch_data = mongoose.connection.db.collection("food_items");
-//         return fetch_data.find({}).toArray(); // Return the promise chain 
-//     })
-//     .then(()=>{
-//         const foodcategory = mongoose.connection.db.collection("foodCategory");
-//         return foodcategory.find({}).toArray();
-//     })
-//     //Handle the promise
-//     .then(data => {
-//         global.food_items = data; // Store the fetched data in the local variable
-//     })
-//     .then(categoryData => {
-//         global.foodcategory = categoryData;
-//     })
-//     .catch(error => {
-//         console.error('Error connecting to MongoDB:', error);
-//     });
-
-//     module.exports = router;
-
 
 
 
